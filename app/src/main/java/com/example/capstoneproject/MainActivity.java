023bf73842b9fc3f9992d95433f2c8b625c14d0b
Jimmy <jwu4477@gmail.com>
2021-10-18 18:32:46 -0400

    Initial commit

---
+++ app/src/main/java/com/example/capstoneproject/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.capstoneproject;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Fragment fragment;
+                switch (menuItem.getItemId()) {
+                    case R.id.action_alerts:
+                        Toast.makeText(MainActivity.this, "Alerts!", Toast.LENGTH_SHORT).show();
+                        //fragment = new AlertsFragment();
+                        break;
+                    case R.id.action_portfolio:
+                        Toast.makeText(MainActivity.this, "Portfolio!", Toast.LENGTH_SHORT).show();
+                        //fragment = new PortfolioFragment();
+                        break;
+                    case R.id.action_news:
+                    default:
+                        Toast.makeText(MainActivity.this, "News!", Toast.LENGTH_SHORT).show();
+                        //fragment = new NewsFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+
+        });
+        // Set default selection
+        bottomNavigationView.setSelectedItemId(R.id.action_alerts);
+    }
+}
