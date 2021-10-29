42b99ebe9a06d522ea120e70bca242f4036ac580
ianzuresgit config --global user.name ianzures <i.anzures514@gmail.com>
2021-10-29 17:56:25 -0400

    Added data persistence to NewsFragment. When user comes back to News tab, onViewCreated() is invoked which makes a json request using sharedPreferences.

--- app/src/main/java/com/example/capstoneproject/fragments/NewsFragment.java
+++ app/src/main/java/com/example/capstoneproject/fragments/NewsFragment.java
@@ -91,7 +91,30 @@ public class NewsFragment extends Fragment {
         requestQueue = Volley.newRequestQueue(this.getContext());
 
         sharedPreferences = getActivity().getSharedPreferences("MyUserPrefs", Context.MODE_PRIVATE);
-        Toast.makeText(getContext(), sharedPreferences.getString("url", ""), Toast.LENGTH_SHORT).show();
+        //Toast.makeText(getContext(), sharedPreferences.getString("url", ""), Toast.LENGTH_SHORT).show();
+
+        JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, sharedPreferences.getString("url", "") , null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            articles.clear();
+                            JSONArray results = response.getJSONArray("data");// results is an array in the json
+                            articles.addAll(Article.fromJsonArray(results));
+                            articleAdapter.notifyDataSetChanged();
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                error.printStackTrace();
+            }
+        });
+
+        requestQueue.add(request);
 
         tvSearch.setOnKeyListener(new View.OnKeyListener(){
             @Override
@@ -99,7 +122,7 @@ public class NewsFragment extends Fragment {
                 // If the event is a key-down event on the "enter" button
                 if ((keyEvent.getAction() == KeyEvent.ACTION_DOWN) &&
                         (i == 66)) {
-                    jsonParse();
+                    jsonParse("");
                     return true;
                 }
 
@@ -125,14 +148,22 @@ public class NewsFragment extends Fragment {
         );
     }
 
-    private void jsonParse(){
-        tickers = tvSearch.getText().toString();;
-        url = String.format("https://stocknewsapi.com/api/v1?tickers=%s,&items=%d&token=i0rpdgcnbrcgaimxbclxhztmuu6sk8jm79zcludj&fbclid=IwAR0pguARasu-pDs_Jcy4Wc4fCL_JIXCjRc_JYwsSN57xOSCnhleL3I2LDHA",tickers,numItems);
+    private void jsonParse(String defURL){
+        tickers = tvSearch.getText().toString();
 
-        // save url in shared preferences
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("url", url);
-        editor.commit();
+        // if tickers is empty, the user must have left the fragment or done something to reinvoke onCreateView()
+        if(tickers == ""){
+            // in this case use the saved url from their previous interaction with this screen
+            Toast.makeText(getContext(), "Empty tickere", Toast.LENGTH_SHORT).show();
+            url = defURL;
+        }else{
+            // else the user must have entered a new set of tickers. save the new url in sharedPreferences
+            url = String.format("https://stocknewsapi.com/api/v1?tickers=%s,&items=%d&token=i0rpdgcnbrcgaimxbclxhztmuu6sk8jm79zcludj&fbclid=IwAR0pguARasu-pDs_Jcy4Wc4fCL_JIXCjRc_JYwsSN57xOSCnhleL3I2LDHA",tickers,numItems);
+            // save url in shared preferences
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString("url", url);
+            editor.commit();
+        }
 
         JsonObjectRequest request = new JsonObjectRequest(Request.Method.GET, url, null,
                 new Response.Listener<JSONObject>() {
@@ -159,7 +190,7 @@ public class NewsFragment extends Fragment {
     }
 
     private void updateOperation(){
-        jsonParse();
+        jsonParse("");
         swipeRefreshLayout.setRefreshing(false);
     }
 
